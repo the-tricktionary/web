2e3460116f87e427f3b76ce123cf476f35c41171
svbeon <svante.b47@gmail.com>
2016-03-29 13:47:01 +0200

    Update mock-tricks.ts

--- app/mock-tricks.ts
+++ app/mock-tricks.ts
@@ -1,14 +1,23 @@
 import { Trick } from './trick';
 
 export var TRICKS: Trick[] = [
-	{"id": 1, "name": "AS"},
-	{"id": 2, "name": "TS"},
-	{"id": 3, "name": "EB"},
-	{"id": 4, "name": "CL"},
-	{"id": 5, "name": "Criss-Cross"},
-	{"id": 6, "name": "Double Under"},
-	{"id": 7, "name": "One-hand Release"},
-	{"id": 8, "name": "TJ"},
-	{"id": 9, "name": "Caboose"},
-	{"id": 10, "name": "Loop"}
+	{"id": 1, "name": "Basics", "subs": [
+	    {"id": 1, "name": "180 Backwards"},
+	    {"id": 2, "name": "180 Forward"},
+	    {"id": 3, "name": "360"},
+	    {"id": 4, "name": "Backwards Jump"},
+	    {"id": 5, "name": "Backwards Side Swing"},
+	    {"id": 6, "name": "Backwards Side Swing Open"},
+	    {"id": 7, "name": "Bell"},
+	    {"id": 8, "name": "Criss Cross"},
+    	    {"id": 9, "name": "Double Bounce"},
+	    {"id": 10, "name": "Double Side Swing"}
+	],
+	{"id": 2, "name": "Level 1", "subs": [
+	    {"id": 1, "name": "Double Under", "type": "Multiple"},
+	    {"id": 2, "name": "Trick", "type": "Multiple"},
+	    {"id": 3, "name": "Trick", "type": "Power"},
+	    {"id": 4, "name": "Trick", "type": "Manipulation"},
+	    {"id": 5, "name": "Trick", "type": "Release"}
+	]
 ];
