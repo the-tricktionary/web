27c7b6d70c03db0dbe4139f9cbb722d883226687
Svante Bengtson <svante.b37@gmail.com>
2016-03-29 08:23:45 +0200

    Add router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,41 @@
 import { Component }       from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 import { TrickService }     from './trick.service';
 import { TricksComponent } from './tricks.component';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <my-tricks></my-tricks>
-  `,
-  directives: [TricksComponent],
+  <h1>{{title}}</h1>
+  <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Tricks']">Tricks</a>
+  </nav>
+  <router-outlet></router-outlet>
+`,
+
+  directives: [ROUTER_DIRECTIVES],
   providers: [
+    ROUTER_PROVIDERS,
     TrickService
   ]
+
 })
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/tricks',
+    name: 'Tricks',
+    component: TricksComponent
+  }
+])
+
+
 export class AppComponent {
   title = 'Tricktionary';
 }
---
+++ app/dashboard.component.ts
@@ -0,0 +1,7 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-dashboard',
+  template: '<h3>Dashboard</h3>'
+})
+export class DashboardComponent { }
