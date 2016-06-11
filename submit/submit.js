bfaff5e095111ee2fef2c6a6f14d8b5182c1661b
Svante Bengtson <svante.b37@gmail.com>
2016-06-11 13:10:27 +0200

    move auth to global

--- submit/submit.js
+++ submit/submit.js
@@ -11,32 +11,11 @@ angular.module('trick.submit', ['ngRoute'])
     }
   ])
   
-  .controller('SubmitCtrl', function($scope, $firebaseArray, $firebaseAuth, Auth) {
-    $scope.signIn = function() {
-      $scope.user = null;
-      $scope.error = null;
-      Auth.$signInWithPopup("google")
-        .then(function(firebaseUser) {
-          $scope.user = firebaseUser;
-        })
-        .catch(function(error) {
-          $scope.error = error;
-        });
-    };
-
-    $scope.signOut = function() {
-      Auth.$signOut();
-    };
-
-    // any time auth status updates, add the user data to scope
-    Auth.$onAuthStateChanged(function(firebaseUser) {
-      $scope.user = firebaseUser;
-    });
-
-    if($scope.user) {
+  .controller('SubmitCtrl', function($scope, $rootScope, $firebaseArray, $firebaseAuth, Auth) {
+    if($rootScope.user) {
       var ref = firebase.database().ref().child("submissions/" + $scope.user.uid);
       // create a synchronized array
-      $scope.user.submissions = $firebaseArray(ref);
+      $rootScope.user.submissions = $firebaseArray(ref);
     }
 
     // connect to storage
--- trick.js
+++ trick.js
@@ -22,4 +22,35 @@ angular.module('trick', [
     function($firebaseAuth) {
       return $firebaseAuth();
     }
+  ])
+
+  .run([
+    '$location', '$rootScope', 'Auth', function($location, $rootScope, Auth) {
+      $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+        if(error === "AUTH_REQUIRED") {
+          $location.path("/dash");
+        }
+      });
+
+      $rootScope.signIn = function() {
+        $rootScope.user = null;
+        $rootScope.error = null;
+        Auth.$signInWithPopup("google")
+          .then(function(firebaseUser) {
+            $rootScope.user = firebaseUser;
+          })
+          .catch(function(error) {
+            $rootScope.error = error;
+          });
+      };
+
+      $rootScope.signOut = function() {
+        Auth.$signOut();
+      };
+
+      // any time auth status updates, add the user data to scope
+      Auth.$onAuthStateChanged(function(firebaseUser) {
+        $rootScope.user = firebaseUser;
+      });
+    }
   ]);
