346d6b4c2241b70e692ed659eb114c7f0e59d8ce
Svante Bengtson <svante.b37@gmail.com>
2016-07-11 16:36:36 +0200

    contact form

--- app/contact/contact.js
+++ app/contact/contact.js
@@ -6,47 +6,58 @@ angular.module('trick.contact', ['ngRoute'])
     '$routeProvider', function($routeProvider) {
       $routeProvider.when('/contact', {
         templateUrl: '/contact/contact.html',
-        controller: 'ContactCtrl',
-        resolve: {
-          "currentAuth": [
-            "Auth", function(Auth) {
-              return Auth.$requireSignIn();
-            }
-          ]
-        }
+        controller: 'ContactCtrl'
       });
     }
   ])
   
-  .controller('SubmitCtrl', function($scope, $firebaseArray, Auth, $location) {
-    Auth.$onAuthStateChanged(function() {
-      if($scope.user) {
-        var ref = firebase.database().ref().child("contact");
-        // create a synchronized array
-        $scope.contact = $firebaseArray(ref);
-
-        // connect to storage
-        //var storageRef = firebase.storage().ref();
-
-        $scope.submit = function() {
-          if(true) {
-            $scope.error = undefined;
-            $scope.contact.$add({
-              name: $scope.newName,
-              desc: $scope.newDesc,
-              type: $scope.newType
-            });
-            $location.path('/');
-            //var folder = storageRef.child('submissions/' + $scope.user.uid + "/" + filename);
-          }
-          else {
-            $scope.error = "You need to enter a value into all fields"
-          }
-        }
+  .controller('ContactCtrl', function($scope, $firebaseArray, Auth) {
+    var ref = firebase.database().ref().child("contact");
+    // create a synchronized array
+    $scope.contact = $firebaseArray(ref);
+    
+    
+    $scope.submitGen = function() {
+      if($scope.user.uid == undefined) {
+        $scope.user.displayName = document.getElementById('name').value;
       }
-      else {
+      var issue = document.getElementById('desc').value;
+      
+      if($scope.user.displayName && issue !== "") {
+        $scope.error = undefined;
+        $scope.contact.$add({
+          name: $scope.user.displayName,
+          desc: issue
+        });
         $location.path('/');
       }
-    })
-  })
-;
+      else {
+        $scope.error = "You need to enter a value into all fields"
+      }
+    };
+    
+    $scope.submitLev = function() {
+      if($scope.user.uid == undefined) {
+        $scope.user.displayName = document.getElementById('name').value;
+      }
+      var newOrg = document.getElementById('org').value;
+      var newId0 = document.getElementById('id0').value;
+      var newId1 = document.getElementById('id1').value;
+      var newLvl = document.getElementById('level').value;
+      
+      if($scope.user.displayName && newId0 && newId1 && newLvl && newOrg) {
+        $scope.error = undefined;
+        $scope.contact.$add({
+          name: $scope.user.displayName,
+          id0: newId0,
+          id1: newId1,
+          level: newLvl,
+          org: newOrg
+        });
+        $location.path('/');
+      }
+      else {
+        $scope.error = "You need to enter a value into all fields"
+      }
+    };
+  });
