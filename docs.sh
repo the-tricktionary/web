04deedfc6c3b7ae376a04b7a40e118f415c700b2
Svante Bengtson <svante.b37@gmail.com>
2016-08-13 22:09:43 +0200

    typo

--- docs.sh
+++ docs.sh
@@ -4,7 +4,7 @@ if [ ! -f deploy.key ]; then exit; fi
 
 export SSH_KEYFILE="$(readlink -f deploy.key)"
 export GIT_SSH="$(readlink -f deploy/ssh.sh)"
-git clone -b gh-pages git@github.com:the-tricktionary/web .deploy || exit 1
+git clone -b gh-pages git@github.com:the-tricktionary/web.git .deploy || exit 1
 cd .deploy || exit 1
 git config user.name travis
 git config user.email travis@nowhere
