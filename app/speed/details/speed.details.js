936b8eb33dd6f8775c1f320babff55314783b1a3
Svante Bengtson <svante.b37@gmail.com>
2016-08-15 15:03:40 +0200

    charts and resource updates

--- app/speed/details/speed.details.js
+++ app/speed/details/speed.details.js
@@ -30,7 +30,7 @@ angular.module('trick.speed.details', ['ngRoute'])
    * @param {service} $location
    * @param {service} Auth
    */
-  .controller('SpeedDetailsCtrl', function($scope, $firebaseObject, $routeParams, $location, Auth) {
+  .controller('SpeedDetailsCtrl', function($scope, $firebaseObject, $routeParams, $location, $filter, Auth) {
     Auth.$onAuthStateChanged(function() {
       if($scope.user) {
         /**
@@ -54,6 +54,87 @@ angular.module('trick.speed.details', ['ngRoute'])
          * @description get the service $location into scope for us in template
          */
         $scope.$location = $location;
+        
+        // TODO: Parse data in event.chartData to output labels
+        var parse = function(parseData, duration) {
+          var outData = {
+            labels: [],
+            series: [
+              []
+            ]
+          };
+          var parseDataLength = parseData.length;
+          for(var i = 0; i < parseDataLength; i++) {
+            outData.series[0].push(100 * (1 / (parseData[i] - parseData[i - 1])));
+            outData.labels.push($filter('date')((duration / parseDataLength * i * 1000), 'mm:ss'));
+          }
+          outData.labels.shift();
+          outData.series[0].shift();
+          return outData;
+        };
+        
+        var arrayMax = function(array) {
+          return array.reduce(function(a, b) {
+            return Math.max(a, b);
+          });
+        };
+        
+        $scope.event.$watch(function() {
+          var chartData = parse($scope.event.graphData, $scope.event.time);
+          var chartOptions = {
+            chartPadding: {
+              top: 20,
+              right: 0,
+              bottom: 30,
+              left: 0
+            },
+            showArea: true,
+            showLine: true,
+            showPoint: false,
+            high: arrayMax(chartData.series[0]) + 2,
+            axisX: {
+              labelInterpolationFnc: function(value, index) {
+                return index % 10 === 0 ? value : null;
+              }
+            },
+            onlyInteger: true,
+            plugins: [
+              Chartist.plugins.ctAxisTitle({
+                axisX: {
+                  axisTitle: 'Time',
+                  axisClass: 'ct-axis-title',
+                  offset: {
+                    x: 0,
+                    y: 50
+                  },
+                  textAnchor: 'middle'
+                },
+                axisY: {
+                  axisTitle: 'Jumps',
+                  axisClass: 'ct-axis-title',
+                  offset: {
+                    x: 0,
+                    y: 0
+                  },
+                  textAnchor: 'middle',
+                  flipTitle: false
+                }
+              })
+            ]
+          };
+          var chartOptionsResponsive = [
+            [
+              'screen and (max-width: 800px)', {
+              axisX: {
+                labelInterpolationFnc: function(value, index) {
+                  return index % 17 === 0 ? value : null;
+                }
+              }
+            }
+            ]
+          ];
+          new Chartist.Line('.ct-chart', chartData, chartOptions, chartOptionsResponsive);
+        })
       }
       else {
         $location.path('/');
