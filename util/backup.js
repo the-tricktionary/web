216c3ce8c23ec7a3740672ecc9ab35e07a71cb1e
Svante Bengtson <svante.b37@gmail.com>
2016-12-28 00:11:23 +0100

    finalize new backup and email

--- util/backup.js
+++ util/backup.js
@@ -1,21 +1,33 @@
-var firebase = require("firebase");
-//var gcloud = require('gcloud')({ 
-  projectId: 'project-5641153190345267944',
-  keyFilename: 'tricktionary-key.json'
-});
-//var fs = require('fs');
+var admin  = require("firebase-admin");
+var fs     = require("fs");
+var moment = require("moment");
 
-// init storage
-//var gcs = gcloud.storage();
-//var bucket = gcs.bucket('project-5641153190345267944.appspot.com');
-// init db
-firebase.initializeApp({
-  serviceAccount: "tricktionary-key.json",
+var serviceAccount = require("./email-data/firebase-adminsdk.json");
+
+// initialize firebase app
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://project-5641153190345267944.firebaseio.com"
 });
 
-// get all data
-var db = firebase.database();
-console.log(db);
+// Get current datetime
+var now = moment().format("YYYYMMDD-HHmmss");
+
+console.log("backing up db" + now);
+
+// create db reference to contacts
+var db  = admin.database();
 var ref = db.ref("/");
-console.log(ref);
+
+ref.on("value", function(data) {
+  fs.writeFile('./backups/backup-' + now + '.json', JSON.stringify(data.val()), function(err) {
+    if (err) throw err;
+    console.log("backup saved");
+    process.exit()
+  });
+})
+
+setTimeout(function() {
+  console.log("backup failed, timeout");
+  process.exit(1);
+}, 60000);
