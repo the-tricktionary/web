6d0413ee2e9a0920e3a683aee7d182e1790769d9
Svante Bengtson <svante.b37@gmail.com>
2017-01-14 22:18:19 +0100

    v1 of the booklet generator

--- util/booklet.js
+++ util/booklet.js
@@ -1,17 +1,24 @@
 var admin  = require("firebase-admin");
 var fs     = require("fs");
 var moment = require("moment");
-var sys = require('sys')
-var exec = require('child_process').exec;
+var exec   = require("child_process").exec;
+//var gcloud = require("google-cloud");
 var child;
+var papersize;
 
 var serviceAccount = require("./email-data/firebase-adminsdk.json");
 
-if (process.argv[2] == "test") {
+if (process.argv.indexOf("test") !== -1) {
   function dlog(msg) { console.log(msg) }
   dlog("running in debug mode")
 } else {
   function dlog(msg) { return true; }
+}
+
+if (process.argv.indexOf("letter") !== -1) {
+  papersize = "letter"
+} else {
+  papersize = "a4"
 }
 
 // initialize firebase app
@@ -25,7 +32,7 @@ dlog("init done");
 // Get current datetime
 var now = moment().format("YYYYMMDD-HHmmss");
 
-var filename = "booklet-" + now;
+var filename = "booklet-" + now + "-" + papersize;
 
 dlog("creating " + filename);
 
@@ -33,23 +40,139 @@ dlog("creating " + filename);
 var db  = admin.database();
 var ref = db.ref("/");
 
-ref.on("value", function(data) {
-  // construct tex file
+ref.on("value", function(snapshot) {
+  var keys = [];
+  var data = snapshot.val().tricks;
+  var types = snapshot.val().tricktypes;
+
+  // construct tex file contents
+  var tex = "";
+  tex += '\\documentclass[12pt]{article}\n'
   
-  
-   fs.writeFile('./booklets/' + filename + '.tex', texcont, function(err) {
-    if (err) throw err;
-    dlog("tex booklet saved");
-    //exec pdflatex
-    child = exec("pdflatex -synctex=1 -interaction=nonstopmode " + filename + ".tex", function (error, stdout, stderr) {
-      dlog('stdout: ' + stdout);
-      dlog('stderr: ' + stderr);
-      if (error !== null) {
-        dlog('exec error: ' + error);
+  if (papersize == "letter" ) {
+    tex += '\\usepackage[paperheight=5.5in,paperwidth=8.5in]{geometry}\n'
+  } else {
+    tex += '\\usepackage[a5paper]{geometry}\n'
+  }
+
+  tex += '\\usepackage{enumitem,amssymb,tabularx}\n'
+  tex += '\\newlist{todolist}{itemize}{2}\n'
+  tex += '\\setlist[todolist]{label=$\\square$,leftmargin=0pt,itemsep=0pt,parsep=0pt}\n'
+  tex += '\\title{the Tricktionary}\n'
+  tex += '\\begin{document}\n'
+  tex += '\\clearpage\\maketitle\n'
+  tex += '\\thispagestyle{empty}\n'
+  tex += '\\vfill\n'
+  tex += '\\begin{small}\n'
+  tex += '\\noindent detailed information about tricks are \\\\\n'
+  tex += 'avilable on the-tricktionary.com or \\\\\n'
+  tex += 'in the Tricktionary\'s android app.\n'
+  tex += '\\end{small}\n'
+  tex += '\\pagebreak\n'
+
+  // tricks
+  var keys = Object.keys(data);
+  keys.forEach(function(key) {
+    leveltypes = {};
+
+    tex += '\\section*{Level ' + data[key].level + '}\n'
+
+    types.forEach(function(type) {
+      Object.keys(data[key].subs).forEach(function(subKey) {
+        if (data[key].subs[subKey].type == type) {
+          if (!leveltypes[type]) {
+            tex += '\\subsection*{' + type + '}\n'
+            tex += '\\begin{todolist}\n'
+            leveltypes[type] = true;
+          }
+          tex += '\\item ' + data[key].subs[subKey].name + '\n'
+        }
+      })
+      if(leveltypes[type]) {
+        tex += '\\end{todolist}\n'
       }
+    })
+    
+    tex += '\\pagebreak\n'
+  })
+
+  // speed sheets
+  for (var pages = 0; pages < 4; pages++) {
+    tex += '\\section*{Speed - }\n'
+    tex += '\\noindent \\begin{tabularx}{\\linewidth}{|X|X|}\n'
+    tex += '\\hline\n'
+    tex += 'Date & Count \\\\\n'
+    tex += '\\hline\n'
+    for (var lines = 0; lines < 26; lines++) {
+      tex += ' & \\\\\n'
+      tex += '\\hline\n'
+    }
+    tex += '\\end{tabularx}\n'
+    if (pages !== 3) {
+      tex += '\\pagebreak\n'
+    }
+    if (pages == 3) {
+    tex += '\\vfill\n'
+    tex += '\\begin{tiny}\n'
+    tex += '\\copyright the Tricktionary 2016-' + moment().format('YYYY') + '\n'
+    tex += '\\end{tiny}\n'
+    }
+
+  }
+
+  tex += '\\end{document}\n'
+
+  // tex for booklet version
+  var bookletTex = "";
+  bookletTex += '\\documentclass[12pt]{article}\n'
+
+  if(papersize == "letter") {
+    bookletTex += '\\usepackage[letterpaper]{geometry}\n'
+  } else {
+    bookletTex += '\\usepackage[a4paper]{geometry}\n'
+  }
+
+  bookletTex += '\\usepackage{pdfpages}\n'
+  bookletTex += '\\includepdfset{pages=-}\n'
+  bookletTex += '\\title{the Tricktionary}\n'
+  bookletTex += '\\begin{document}\n'
+  bookletTex += '\\includepdf[pages=-,landscape,booklet=true]{booklets/raw-' + filename +'.pdf}\n'
+  bookletTex += '\\end{document}\n'
+
+  fs.writeFile('./booklets/raw-' + filename + '.tex', tex, function(err) {
+    if (err) throw err;
+    dlog("tex rawbooklet saved");
+    fs.writeFile('./booklets/' + filename + '.tex', bookletTex, function(err) {
+      if (err) throw err;
+      dlog("tex booklet saved");
+      dlog("generating booklet pdf")
+      //exec pdflatex on raw
+      child = exec("pdflatex -output-directory=booklets -synctex=1 -interaction=nonstopmode ./booklets/raw-" + filename + ".tex", function (error, stdout, stderr) {
+        dlog('stdout: ' + stdout);
+        dlog('stderr: ' + stderr);
+        if (error !== null) {
+          dlog('exec error: ' + error);
+        }
+        if (error === null) {
+          dlog("rawbooklet pdf generated");
+          dlog("generating booklet pdf");
+          child2 = exec("pdflatex -output-directory=booklets -synctex=1 -interaction=nonstopmode ./booklets/" + filename + ".tex", function(error, stdout, stderr) {
+            dlog('stdout: ' + stdout);
+            dlog('stderr: ' + stderr);
+            if (error !== null) {
+              dlog('exec error: ' + error);
+            }
+            if (error === null) {
+              dlog("booklet pdf generated");
+              // upload bookelt pdf to firebase storage
+              process.exit();            
+            }
+          })
+        }
+      });
     });
   });
-}
+});
 
 setTimeout(function() {
   console.log("backup failed, timeout");
