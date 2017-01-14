4bd346bc9be398ec1e860285c8b5298ad7912f4d
Svante Bengtson <svante.b37@gmail.com>
2017-01-14 16:09:31 +0100

    (auto)update (c) year on /about, fixes #32 state that levels aren't verified by certified judges, fixes #31 highlight an issue by adding `?i=[...]` to /contact, fixes #30 add email to /about, fixes #27

--- app/about/about.js
+++ app/about/about.js
@@ -5,33 +5,37 @@
  * @requires ngRoute
  */
 angular.module('trick.about', ['ngRoute'])
-  
-  .config([
-    '$routeProvider', function($routeProvider) {
-      $routeProvider.when('/about', {
-        templateUrl: '/about/about.html',
-        controller: 'AboutCtrl'
-      });
+
+.config([
+    '$routeProvider',
+  function($routeProvider) {
+    $routeProvider.when('/about', {
+      templateUrl: '/about/about.html',
+      controller: 'AboutCtrl'
+    });
     }
   ])
 
+/**
+ * @class trick.about.AboutCtrl
+ * @param {service} $scope
+ * @param {service} $location
+ * @param {service} $anchorScroll
+ * @param {service} Db
+ */
+.controller('AboutCtrl', function($scope, $location, $anchorScroll) {
   /**
-   * @class trick.about.AboutCtrl
-   * @param {service} $scope
-   * @param {service} $location
-   * @param {service} $anchorScroll
-   * @param {service} Db
+   * @name $scope.anchor
+   * @function
+   * @memberOf trick.about.AboutCtrl
+   * @description Store URL's anchor value (`#disclaimer` for example) in the scope
    */
-  .controller('AboutCtrl', function($scope, $location, $anchorScroll) {
-    /**
-     * @name $scope.anchor
-     * @function
-     * @memberOf trick.about.AboutCtrl
-     * @description Store URL's anchor value (`#disclaimer` for example) in the scope
-     */
-    $scope.anchor = $location.hash();
-    /** Configure $anchorScroll to take the navbar into consideration*/
-    $anchorScroll.yOffset = 40;
-    /** Scroll To anchor */
-    $anchorScroll();
-  });
+  $scope.anchor = $location.hash();
+  /** Configure $anchorScroll to take the navbar into consideration*/
+  $anchorScroll.yOffset = 40;
+  /** Scroll To anchor */
+  $anchorScroll();
+
+  $scope.year = new Date()
+    .getFullYear();
+});
