c8ea8fe81c01f6861f05947a6adbd1fb82835616
Svante Bengtson <svante.b37@gmail.com>
2018-12-23 21:23:01 +0100

    forgot a file

---
+++ src/typings/tricks.d.ts
@@ -0,0 +1,29 @@
+interface Trick {
+  by: string;
+  description: string;
+  level: number;
+  name: string;
+  oldid?: number;
+  slowMoStart?: number;
+  slug: string;
+  type: string;
+  id: string;
+
+  videos: any;
+  i18n: any;
+  levels: any;
+}
+
+interface Level {
+  name: number;
+  types: Type[];
+}
+
+interface Type {
+  name: string;
+  tricks: Trick[];
+}
+
+interface ListOfTricks {
+  [prop: string]: Trick;
+}
