cc57d4973bc5debc3fff72fd8a349ff7ec0721ea
Svante Bengtson <svante.b37@gmail.com>
2018-12-31 16:22:15 +0100

    oh right, inaccurate docs

--- src/store/modules/tricksSRModule.ts
+++ src/store/modules/tricksSRModule.ts
@@ -8,7 +8,7 @@ export default {
   },
   fetch: {
     // The max amount of documents to be fetched. Defaults to 50.
-    docLimit: 0
+    docLimit: 5000
   }
   // you can also add state/getters/mutations/actions
   // for other config like fillables see 'Extra features'
