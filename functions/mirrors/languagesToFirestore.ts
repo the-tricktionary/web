9195884486ab181ad22613be96e1938063514232
Svante Bengtson <svante.b37@gmail.com>
2020-07-14 20:43:49 +0200

    move functions to the-tricktionary/functions

--- functions/mirrors/languagesToFirestore.ts
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin')
-const firestore = admin.firestore()
-
-module.exports = (change, context) => {
-  return firestore.collection('i18n').doc(context.params.lang).set({ name: change.after.val(), enabled: true }, { merge: true })
-}
