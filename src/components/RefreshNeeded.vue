735f9f9663dd0e2b61dc31247a4870ad1a6ebbf5
Svante Bengtson <svante@swantzter.se>
2021-06-29 17:50:39 +0000

    Polish refresh needed prompt

--- src/components/RefreshNeeded.vue
+++ src/components/RefreshNeeded.vue
@@ -1,9 +1,9 @@
 <template>
-  <div v-if="needRefresh" class="fixed bottom-0 right-0 left-0 border-t bg-white border-gray-300 pb-4 overflow-x-auto">
-    <div class="container mx-auto px-2 flex items-baseline justify-between">
+  <div v-if="needRefresh" class="fixed bottom-0 right-0 left-0 border-t bg-white border-gray-300 py-2 overflow-x-auto z-10000">
+    <div class="container mx-auto px-2 flex flex-wrap items-baseline gap-2 justify-between">
       <p>New version available, click on reload to update.</p>
 
-      <div class="flex gap-4 flex-nowrap mt-2">
+      <div class="flex gap-4 flex-nowrap">
         <button class="btn w-full sm:w-auto border border-green-500" @click="updateSW()">Reload</button>
         <button class="btn w-full sm:w-auto" @click="dismiss()">Dismiss</button>
       </div>
